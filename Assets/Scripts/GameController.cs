6320016b9d4c5fbe1c569db3bff8ad9eda650815
aaront97 <33289351+aaront97@users.noreply.github.com>
2018-12-01 15:30:39 +0000

    added spawn random objects

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -10,6 +10,7 @@ public class GameController : MonoBehaviour {
     public GameObject Wall;
     public GameObject Hole;
     public GameObject Spikes;
+    public long frameCounter = 0;
     // Use this for initialization
     void Start () {
         Vector3 spawnLoc = getLeftOfScreen(Lane.A);
@@ -18,8 +19,48 @@ public class GameController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
+        frameCounter++; 
+        if(frameCounter == 20000)
+        {
+            spawnRandObjects();
+            frameCounter = 0;
+        }
 	}
+
+    private void spawnRandObjects()
+    {
+        GameObject obj;
+        long objectQuantifier = Random.Range(0, 3);
+        
+        //choose which object type (uniform for now)
+        if (objectQuantifier <= 1.0)
+        {
+            obj = Wall;
+        }
+        else if (objectQuantifier <= 2.0)
+        {
+            obj = Hole;
+        }
+        else
+        {
+            obj = Spikes;
+        }
+        Vector3 spawnLoc;
+
+        //choose which lane
+        if (Random.Range(0, 1) <= 0.5)
+        {
+            spawnLoc = getLeftOfScreen(Lane.A);
+        }
+        else
+        {
+            spawnLoc = getLeftOfScreen(Lane.B);
+        }
+        Track.instance.spawnObstacle(obj, spawnLoc);
+
+    }
+
+
     private Vector3 getLeftOfScreen(Lane lane)
     {
         Vector3 cameraPosition = Camera.main.ViewportToWorldPoint(new Vector2(1.0f, 0f));
@@ -36,3 +77,4 @@ public class GameController : MonoBehaviour {
         return cameraPosition;
     }
 }
+
