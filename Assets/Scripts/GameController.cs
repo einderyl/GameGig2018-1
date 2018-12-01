31b659fd6f75709c9dd8a74ce1e1b33414c7af8b
Kocolipy <33294859+Kocolipy@users.noreply.github.com>
2018-12-01 12:16:03 +0000

    Track
    Added spawn function

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    private Vector2 laneA = new Vector2(1.0f, 2.5f);
+    private Vector2 laneB = new Vector2(1.0f, -2.5f);
+
+    // Use this for initialization
+    void Start () {
+        Track.instance.spawnWall(laneB);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Track.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Track : MonoBehaviour {
+
+    public static Track instance;
+    public GameObject Wall;
+   
+    // Use this for initialization
+    void Awake () {
+        if (instance == null) instance = this;
+        else if (instance != this) Destroy(this);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void spawnWall(Vector2 loc)
+    {
+        spawnObstacle(Wall, loc);
+    }
+
+    private void spawnObstacle(GameObject obstacle, Vector2 loc)
+    {
+        Instantiate(obstacle, loc, Quaternion.identity);
+    }
+}
