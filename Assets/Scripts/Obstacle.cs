c5cdeef1f2c6c704cd697b829d0fd96af0179982
Chuan Tan <ct538@cam.ac.uk>
2018-12-01 13:20:05 +0000

    Added obstacles and a player to play with

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+    // On Collision with any of the players
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "Player")
+        {
+            var hit = other.gameObject;
+            var health = other.gameObject.GetComponent<Player>();
+            health.TakeDamage(100);
+            other.gameObject.GetComponent<Player>().slowDown(50f);
+
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+    public const int maxHealth = 100;
+    public int currentHealth = maxHealth;
+    public RectTransform healthBar;
+    public float _speed = 2.5f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += Vector3.up * _speed * Time.deltaTime;
+    }
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            Debug.Log("Dead!");
+        }
+        healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+    }
+    public void slowDown(float percent)
+    {
+        _speed = percent / 100.0f * _speed;
+    }
+}
